9849952c7ab0d959a875639c05d993b6c8aa726a
gnoooo74 <gnoooo74@github.com>
2026-06-28 03:27:42 +0000

    fix: activity-ktx 추가 (viewModels 델리게이트 누락)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,10 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
+    // Activity KTX (viewModels() 델리게이트)
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
     // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
